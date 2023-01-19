6f77a1312637f550ac7cff566ea82ad454436222
Aelrue <101987454+Aelrue@users.noreply.github.com>
2023-01-19 10:09:30 -0600

    added photos to gallery and added the info to galleryItems, changed port and proxy to 5001, created GalleryList and GalleryItem components

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,38 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+  {
+    id: 1,
+    path: "images/baking.jpg",
+    description:
+      "Freshly baked apple turnovers made using the apples from the tree in our backyard.",
+    likes: 0,
+  },
+  {
+    id: 2,
+    path: "images/travel.jpg",
+    description:
+      "Taken from my seat on the plane as we flew out to Seattle, WA.",
+    likes: 0,
+  },
+  {
+    id: 3,
+    path: "images/geordi.jpg",
+    description: "Our dog, Geordi. ",
+    likes: 0,
+  },
+  {
+    id: 4,
+    path: "images/kurt.jpg",
+    description:
+      "My husband, Kurt. In front of him is a flight of different beers he was trying at a brewery in Seattle, WA.",
+    likes: 0,
+  },
+  {
+    id: 5,
+    path: "images/cats.jpg",
+    description:
+      "Our two cats: our grey tabby named Jaskier and our orange tabby named Wesley.",
+    likes: 0,
+  },
 ];
 
 module.exports = galleryItems;
