d270eb571296fd98e7513510b4d1848fa5bb1893
Aelrue <101987454+Aelrue@users.noreply.github.com>
2023-01-23 15:23:05 -0600

    got description displaying on click

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,19 +1,35 @@
 import react, { useState } from "react";
 import axios from "axios";
 
-function GalleryItem({ photo, fetchGallery, handleClick }) {
+function GalleryItem({ photo, handleClick }) {
   console.log("hello", photo);
-  handleClick(photo);
+  const [isClicked, setIsClicked] = useState(false);
 
-  const isClicked = false;
+  const togglePhoto = () => {
+    console.log("clicked", isClicked);
+    setIsClicked(!isClicked);
+  };
+
+  //   const isClicked = useState(false);
+
+  //   if (isClicked) {
+  //     return <div>{photo.description}</div>;
+  //   } else
+  //     return (
+  //       <div>
+  //         <img src={photo.path} onClick={() => handleClick(photo)} />
+  //       </div>
+  //     );
+  // }
+
   if (isClicked) {
     return <div>{photo.description}</div>;
-  }
-  return (
-    <div>
-      <img src={photo.path} />
-    </div>
-  );
+  } else
+    return (
+      <div>
+        <img src={photo.path} onClick={() => togglePhoto()} />
+      </div>
+    );
 }
 
 export default GalleryItem;
